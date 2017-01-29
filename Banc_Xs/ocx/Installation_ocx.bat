b40db5b0c874772a7064d468a9b6f456452f21c1
anom <patoeng@gmail.com>
2017-01-30 00:08:24 +0700

    add files lab

---
+++ Banc_Xs/ocx/Installation_ocx.bat
@@ -0,0 +1,13 @@
+
+
+cd ..
+
+cd c:\Banc_XS\ocx
+
+cd c:\WINDOWS\System
+
+REGSVR32 c:\Banc_XS\ocx\AfficheProj1.ocx
+REGSVR32 c:\Banc_XS\ocx\Affiche_defaut.dll
+REGSVR32 c:\Banc_XS\ocx\Affiche_message.ocx
+REGSVR32 c:\Banc_XS\ocx\Numerique.ocx
+REGSVR32 c:\Banc_XS\ocx\Threed32.ocx
